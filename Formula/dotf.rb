b733f02dc59d9c9851c2173ac1f7f018027428ea
Andreas Bjoru <andreas.bjoru@gmail.com>
2026-02-21 01:31:18 -0500

    chore(dotf): bump to 3.0.4

--- Formula/dotf.rb
+++ Formula/dotf.rb
@@ -1,9 +1,9 @@
 class Dotf < Formula
   desc "Modular dotfile manager using git bare repo with sparse checkout"
   homepage "https://github.com/abjoru/dotf-v3"
-  version "3.0.3"
+  version "3.0.4"
   url "https://github.com/abjoru/dotf-v3/releases/download/v#{version}/dotf-v#{version}-macos-arm64"
-  sha256 "4e377833fe777e719b35e925b19a6f3c6d1edfb294b025701aac198a90bfbe3d"
+  sha256 "PLACEHOLDER"
   license "BSD-3-Clause"
 
   depends_on :macos
@@ -11,6 +11,41 @@ class Dotf < Formula
 
   def install
     bin.install "dotf-v#{version}-macos-arm64" => "dotf"
+
+    (zsh_completion/"_dotf").write <<~ZSH
+      #compdef dotf
+
+      local request
+      local completions
+      local word
+      local index=$((CURRENT - 1))
+
+      request=(--bash-completion-enriched --bash-completion-index $index)
+      for arg in ${words[@]}; do
+        request=(${request[@]} --bash-completion-word $arg)
+      done
+
+      IFS=$'\\n' completions=($( $(command -v dotf) "${request[@]}" ))
+
+      for word in $completions; do
+        local -a parts
+
+        # Split the line at a tab if there is one.
+        IFS=$'\\t' parts=($( echo $word ))
+
+        if [[ -n $parts[2] ]]; then
+           if [[ $word[1] == "-" ]]; then
+             local desc=("$parts[1] ($parts[2])")
+             compadd -d desc -- $parts[1]
+           else
+             local desc=($(print -f  "%-019s -- %s" $parts[1] $parts[2]))
+             compadd -l -d desc -- $parts[1]
+           fi
+        else
+          compadd -f -- $word
+        fi
+      done
+    ZSH
   end
 
   test do
